c19a29941c07cec54ced2e3d50555f75adf2ec52
jbucanay <gilbert.baracka@gmail.com>
2022-02-09 22:38:52 -0700

    done

--- process_tweets.py
+++ process_tweets.py
@@ -5,22 +5,46 @@
 #tweets that mention neither
 
 import json
-import string
 
-def has_rams(str):
-    rams_count = 0
+
+def has_rams(strng):
+    lower_str = strng.lower()
+    return lower_str.count('rams')
     
-def has_pats(str):
-    pats_count = 0
+def has_pats(strng):
+    lower_str = strng.lower()
+    return lower_str.count('patriots')
 
 def main():
     ffile = open('superbowltweets.json', 'r')
     jfile = json.loads(ffile.read())
 
-    print(dir(string))
+    rams_count = 0
+    pats_count = 0
+    both_count = 0
+    neither_count = 0
+    
+    
+    for item in jfile:
+        full_tweet = item['full_text']
+        print(full_tweet)
+        if has_rams(full_tweet) > 0:
+            rams_count += 1
+        if has_pats(full_tweet) > 0:
+            pats_count += 1
+        if has_pats(full_tweet) > 0 and has_rams(full_tweet) > 0:
+            both_count +=1
+        elif has_pats(full_tweet) <= 0 and has_rams(full_tweet) <= 0:
+            neither_count +=1
+    print(f'{rams_count} Tweets mentioned Rams in them')
+    print(f'{pats_count} Tweets mentioned Patriots in them')
+    print(f'{rams_count} Tweets mentioned Rams and Patriots in them')
+    print(f'{neither_count} Tweets did not mentioned Rams nor Patriots ')
 
-    # print(f'this is the first line for testing levels: {jfile[0]["full_text"]}')
-    # print(f'second one hopefully: {jfile[1]["full_text"]}')
+        
+    
+    
+    
 
 
 
